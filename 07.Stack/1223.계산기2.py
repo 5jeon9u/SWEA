9804f1841b76676c71d0aea64997280264dab397
5jeon9u <5jeon9u@gmail.com>
2022-02-23 22:50:35 +0900

    0223 commit

---
+++ 07.Stack/1223.계산기2.py
@@ -0,0 +1,37 @@
+import sys
+sys.stdin = open('1223.txt')
+
+for tc in range(1, 11):
+    N = int(input())
+    cal = input()
+
+    icp = {'+' : 1, '*' : 2}
+    isp = {'+' : 1, '*' : 2}
+
+    stack = []
+    new_cal = []
+
+    for i in cal:
+        if i in icp:
+            while stack and isp[stack[-1]] >= icp[i]:
+                new_cal.append(stack.pop())
+            stack.append(i)
+        else:
+            new_cal.append(i)
+
+    while stack: # 남아있는 연산자 출력
+        new_cal.append(stack.pop())
+
+    for j in new_cal:
+        if j in icp:
+            b = int(stack.pop())
+            a = int(stack.pop())
+            if j == '+':
+                stack.append(a+b)
+            else:
+                stack.append(a*b)
+        else:
+            stack.append(j)
+
+    print(f'#{tc} {stack[0]}')
+
